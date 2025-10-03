31a23d847071ca568d1ca1c604d63bbfbb7bf838
GuyLeigh <dineshvaro357@gmail.com>
2025-10-03 18:11:18 +0000

    Add ABI definitions

---
+++ frontend/src/abi/CardCollectionAddresses.ts
@@ -0,0 +1,7 @@
+export const CardCollectionAddresses = {
+  "11155111": {
+    "address": "0x2babc24fF66c5f5FBaAE39884835CaC41f82E00d",
+    "chainName": "sepolia",
+    "chainId": 11155111
+  }
+} as const;
